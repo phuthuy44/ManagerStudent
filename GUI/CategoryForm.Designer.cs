c921f5eaa6b35221d2ad2aa350f0688825782b74
Hồ Tú Tài <hotutai2002@gmail.com>
2023-10-19 21:34:06 +0700

    update

--- GUI/CategoryForm.Designer.cs
+++ GUI/CategoryForm.Designer.cs
@@ -130,6 +130,8 @@
             this.label12 = new System.Windows.Forms.Label();
             this.label13 = new System.Windows.Forms.Label();
             this.textBox16 = new System.Windows.Forms.TextBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox19 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox21)).BeginInit();
@@ -339,25 +341,27 @@
             // panel3
             // 
             this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel3.Controls.Add(this.textBox19);
+            this.panel3.Controls.Add(this.label14);
             this.panel3.Controls.Add(this.textBox8);
             this.panel3.Controls.Add(this.textBox9);
             this.panel3.Controls.Add(this.label7);
             this.panel3.Controls.Add(this.label8);
             this.panel3.Location = new System.Drawing.Point(27, 93);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(251, 103);
+            this.panel3.Size = new System.Drawing.Size(251, 122);
             this.panel3.TabIndex = 10;
             // 
             // textBox8
             // 
-            this.textBox8.Location = new System.Drawing.Point(103, 57);
+            this.textBox8.Location = new System.Drawing.Point(105, 47);
             this.textBox8.Name = "textBox8";
             this.textBox8.Size = new System.Drawing.Size(129, 20);
             this.textBox8.TabIndex = 5;
             // 
             // textBox9
             // 
-            this.textBox9.Location = new System.Drawing.Point(103, 13);
+            this.textBox9.Location = new System.Drawing.Point(105, 13);
             this.textBox9.Name = "textBox9";
             this.textBox9.Size = new System.Drawing.Size(129, 20);
             this.textBox9.TabIndex = 4;
@@ -366,21 +370,21 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(13, 57);
+            this.label7.Location = new System.Drawing.Point(3, 47);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 16);
+            this.label7.Size = new System.Drawing.Size(88, 16);
             this.label7.TabIndex = 1;
-            this.label7.Text = "Tên kết quả";
+            this.label7.Text = "Điểm cận trên";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(13, 14);
+            this.label8.Location = new System.Drawing.Point(3, 14);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(73, 16);
+            this.label8.Size = new System.Drawing.Size(81, 16);
             this.label8.TabIndex = 0;
-            this.label8.Text = "Mã kết quả";
+            this.label8.Text = "Tên xếp loại";
             // 
             // dataGridView1
             // 
@@ -561,15 +565,16 @@
             this.panel1.Controls.Add(this.label2);
             this.panel1.Location = new System.Drawing.Point(24, 98);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(251, 103);
+            this.panel1.Size = new System.Drawing.Size(251, 130);
             this.panel1.TabIndex = 11;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(103, 57);
+            this.textBox3.Location = new System.Drawing.Point(109, 58);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(129, 20);
             this.textBox3.TabIndex = 5;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // textBox4
             // 
@@ -582,7 +587,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 57);
+            this.label1.Location = new System.Drawing.Point(8, 58);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 16);
             this.label1.TabIndex = 1;
@@ -1279,6 +1284,23 @@
             this.textBox16.Size = new System.Drawing.Size(139, 34);
             this.textBox16.TabIndex = 8;
             // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(3, 78);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(92, 16);
+            this.label14.TabIndex = 6;
+            this.label14.Text = "Điểm cận dưới";
+            // 
+            // textBox19
+            // 
+            this.textBox19.Location = new System.Drawing.Point(105, 77);
+            this.textBox19.Name = "textBox19";
+            this.textBox19.Size = new System.Drawing.Size(129, 20);
+            this.textBox19.TabIndex = 7;
+            // 
             // CategoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -1451,5 +1473,7 @@
         private System.Windows.Forms.PictureBox pictureBox23;
         private System.Windows.Forms.PictureBox pictureBox24;
         private System.Windows.Forms.PictureBox pictureBox25;
+        private System.Windows.Forms.TextBox textBox19;
+        private System.Windows.Forms.Label label14;
     }
 }
