ff9c79e0af66486f1a497f2407da233e891cf486
ngxthanhluc <ngthanhluc.0504@gmail.com>
2023-11-19 00:58:07 +0700

    up det

--- BLL/ConductBLL.cs
+++ BLL/ConductBLL.cs
@@ -24,5 +24,34 @@ namespace ManagerStudent.BLL
         public DataTable FindConduct(string str) {
             return ConductData.FindConduct(str);
         }
+        public bool insertConducts(string conductName, int upperLimit, int lowerLimit)
+        {
+            GetConductData insertConductData = new GetConductData();
+            return insertConductData.insertConduct(conductName, upperLimit, lowerLimit);
+        }
+
+        public bool deleteConducts(string conductName)
+        {
+            GetConductData deleteConductData = new GetConductData();
+            return deleteConductData.deleteConduct(conductName);
+        }
+
+        public bool updateConducts(string conductName, int upperLimit, int lowerLimit, int ID)
+        {
+            GetConductData updateConductData = new GetConductData();
+            return updateConductData.updateConduct(conductName, upperLimit, lowerLimit, ID);
+        }
+
+        public bool checkUpdateConductName(string conductName, int ID)
+        {
+            GetConductData checkUpdateConductData = new GetConductData();
+            return checkUpdateConductData.checkUpdateConductName(conductName, ID);
+        }
+
+        public bool checkInsertConductName(string conductName)
+        {
+            GetConductData checkInsertConductData = new GetConductData();
+            return checkInsertConductData.checkInsertConductName(conductName);
+        }
     }
 }
